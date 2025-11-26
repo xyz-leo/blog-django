59f134a50b3d31949061424344401b8232c43cfe
xyzleo <xyz-leo>
2025-11-26 15:38:01 -0300

    The application now incorporates two important structural improvements: centralized loading of environment variables via python-dotenv, ensuring that sensitive settings remain outside the code, and the integration of django-axes, which adds a layer of security to the authentication process, limiting failed attempts and applying temporary locks. These two additions strengthen the runtime environment and increase the application's resilience.

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -13,11 +13,18 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+
 
 DEBUG = bool(int(os.environ.get("DEBUG", default=0)))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# DOTENV
+load_dotenv(BASE_DIR.parent / 'dotenv' / '.env', override=True)
+
 # /data/web/static
 # /data/web/media
 DATA_DIR = BASE_DIR.parent / "data" / "web"
@@ -50,6 +57,9 @@ INSTALLED_APPS = [
     
     # Summernote
     'django_summernote',
+
+    # django-axes
+    'axes',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +70,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # AxesMiddleware should be the last middleware in the MIDDLEWARE list.
+    # It only formats user lockout messages and renders Axes lockout responses
+    # on failed user authentication attempts from login views.
+    # If you do not want Axes to override the authentication response
+    # you can skip installing the middleware and use your own views.
+    'axes.middleware.AxesMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -144,6 +161,13 @@ MEDIA_ROOT = DATA_DIR / "media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTHENTICATION_BACKENDS = [
+    # AxesStandaloneBackend should be the first backend in the AUTHENTICATION_BACKENDS list.
+    'axes.backends.AxesStandaloneBackend',
+
+    # Django ModelBackend is the default authentication backend.
+    'django.contrib.auth.backends.ModelBackend',
+]
 
 # Summernote config
 SUMMERNOTE_CONFIG = {
@@ -172,3 +196,9 @@ SUMMERNOTE_CONFIG = {
     'attachment_filesize_limit': 30 * 1024 * 1024,
     'attachment_model': 'blog.PostAttachment',
 }
+
+# Axes config
+AXES_ENABLED = True
+AXES_FAILURE_LIMIT = 3
+AXES_COOLOFF_TIME = 3  # 3 hours
+AXES_RESET_ON_SUCCESS = True
--- djangoapp/project/wsgi.py
+++ djangoapp/project/wsgi.py
@@ -11,6 +11,15 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from pathlib import Path
+from dotenv import load_dotenv
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# DOTENV
+load_dotenv(BASE_DIR.parent / 'dotenv' / '.env', override=True)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
